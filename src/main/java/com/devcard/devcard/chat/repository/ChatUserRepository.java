48bd652a293cff28f14bbe6f067bac1deaf45bda
pswaao88 <66404498+pswaao88@users.noreply.github.com>
2024-09-27 17:41:31 +0900

    feat: add ChatUserRepository and remove code in chatRoonRepository

---
+++ src/main/java/com/devcard/devcard/chat/repository/ChatUserRepository.java
@@ -0,0 +1,9 @@
+package com.devcard.devcard.chat.repository;
+
+import com.devcard.devcard.chat.model.ChatUser;
+import java.util.List;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ChatUserRepository extends JpaRepository<ChatUser, Long> {
+    List<ChatUser> findByIdIn(List<Long> participantsId);
+}
