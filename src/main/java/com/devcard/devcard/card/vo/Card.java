f4d142027123367b1af4450e58ac5840a0022ed6
donghyeon <thornappl2@gmail.com>
2024-09-27 14:32:21 +0900

    feat: delete setter in VO

--- src/main/java/com/devcard/devcard/card/vo/Card.java
+++ src/main/java/com/devcard/devcard/card/vo/Card.java
@@ -1,7 +1,6 @@
 package com.devcard.devcard.card.vo;
 
 import jakarta.persistence.*;
-
 import java.time.LocalDateTime;
 
 @Entity
@@ -11,6 +10,7 @@ public class Card {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
     private String githubId;
     private String name;
     private String company;
@@ -19,10 +19,15 @@ public class Card {
     private String phone;
     private String profilePicture;
     private String bio;
+
     private LocalDateTime createdAt;
     private LocalDateTime updatedAt;
     // @Todo add: socialLinks
     // @Todo add: tags
+
+    protected Card() {
+
+    }
 
     public Card(String name, String company, String email, String position, String phone) {
         this.name = name;
@@ -31,95 +36,20 @@ public class Card {
         this.position = position;
         this.phone = phone;
         this.createdAt = LocalDateTime.now();
+        this.updatedAt = LocalDateTime.now();
     }
 
-    public Card() {
-
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public String getGithubId() {
-        return githubId;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public String getCompany() {
-        return company;
-    }
-
-    public String getPosition() {
-        return position;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public String getPhone() {
-        return phone;
-    }
-
-    public String getProfilePicture() {
-        return profilePicture;
-    }
-
-    public String getBio() {
-        return bio;
-    }
-
-    public LocalDateTime getCreatedAt() {
-        return createdAt;
-    }
-
-    public LocalDateTime getUpdatedAt() {
-        return updatedAt;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public void setBio(String bio) {
-        this.bio = bio;
-    }
-
-    public void setProfilePicture(String profilePicture) {
-        this.profilePicture = profilePicture;
-    }
-
-    public void setPhone(String phone) {
-        this.phone = phone;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public void setPosition(String position) {
-        this.position = position;
-    }
-
-    public void setCompany(String company) {
-        this.company = company;
-    }
-
-    public void setGithubId(String githubId) {
-        this.githubId = githubId;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public void setUpdatedAt(LocalDateTime updatedAt) {
-        this.updatedAt = updatedAt;
-    }
+    public Long getId() { return id; }
+    public String getGithubId() { return githubId; }
+    public String getName() { return name; }
+    public String getCompany() { return company; }
+    public String getPosition() { return position; }
+    public String getEmail() { return email; }
+    public String getPhone() { return phone; }
+    public String getProfilePicture() { return profilePicture; }
+    public String getBio() { return bio; }
+    public LocalDateTime getCreatedAt() { return createdAt; }
+    public LocalDateTime getUpdatedAt() { return updatedAt; }
 
     public void update(Card updatedCard) {
         this.name = updatedCard.getName();
