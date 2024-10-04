d8a0d113bbba430751e2db393659c40d5ce998d3
donghyeon <thornappl2@gmail.com>
2024-10-04 22:20:28 +0900

    refactor: Simplify CardController by delegating to service layer
    Modified createCard method to include Location header and return CardResponseDto with appropriate HTTP status codes. Added @Valid annotation to enable request data validation. Clarified the separation of concerns between the controller and service layers by removing unnecessary data conversion in the controller. Improved code readability by removing unused imports and applying consistent coding styles.

--- src/main/java/com/devcard/devcard/card/controller/CardController.java
+++ src/main/java/com/devcard/devcard/card/controller/CardController.java
@@ -4,8 +4,12 @@ import com.devcard.devcard.card.dto.CardResponseDto;
 import com.devcard.devcard.card.entity.Card;
 import com.devcard.devcard.card.dto.CardRequestDto;
 import com.devcard.devcard.card.service.CardService;
+import jakarta.validation.Valid;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import java.net.URI;
 
 @RestController
 @RequestMapping("/cards")
@@ -17,21 +21,27 @@ public class CardController {
         this.cardService = cardService;
     }
 
-    @PostMapping()
-    public void createCard(@RequestBody CardRequestDto cardRequestDto) {
-        cardService.createCard(cardRequestDto.toEntity());
+    @PostMapping
+    public ResponseEntity<CardResponseDto> createCard(@Valid @RequestBody CardRequestDto cardRequestDto) {
+        CardResponseDto responseDto = cardService.createCard(cardRequestDto);
+        URI location = ServletUriComponentsBuilder.fromCurrentRequest()
+                .path("/{id}")
+                .buildAndExpand(responseDto.getId())
+                .toUri();
+        return ResponseEntity.created(location).body(responseDto);
     }
 
+
     @GetMapping("/{id}")
-    public ResponseEntity<CardResponseDto> getCard(@PathVariable(name="id") Long id) {
-        Card card = cardService.getCard(id);
-        return ResponseEntity.ok(CardResponseDto.fromEntity(card));
+    public ResponseEntity<CardResponseDto> getCard(@PathVariable Long id) {
+        CardResponseDto responseDto = cardService.getCard(id);
+        return ResponseEntity.ok(responseDto);
     }
 
     @PutMapping("/{id}")
-    public ResponseEntity<CardResponseDto> updateCard(@PathVariable Long id, @RequestBody CardRequestDto cardRequestDto) {
-        Card updatedCard = cardService.updateCard(id, cardRequestDto.toEntity());
-        return ResponseEntity.ok(CardResponseDto.fromEntity(updatedCard));
+    public ResponseEntity<CardResponseDto> updateCard(@PathVariable Long id, @Valid @RequestBody CardRequestDto cardRequestDto) {
+        CardResponseDto responseDto = cardService.updateCard(id, cardRequestDto);
+        return ResponseEntity.ok(responseDto);
     }
 
     @DeleteMapping("/{id}")
